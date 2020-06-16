f357e9b371fce07d5ac0f6915cb888f0e0ac9f1c
Russ Perry <rperry1886@gmail.com>
2020-06-16 19:35:17 -0400

    Add functions
    Function to add tiles, Function to create array of colors

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 $(document).ready(function () {
   $("#begin").click(() => {
-    $("#game").removeClass("hide");
-    $("#start").addClass("hide");
+    newGame();
+    appendTiles(3);
   });
 });
 
-function resetGame() {
+function gameOver() {
   $("#start").removeClass("hide");
   $("#game").addClass("hide");
 }
+
+function newGame() {
+  $("#game").removeClass("hide");
+  $("#start").addClass("hide");
+}
+
+//TODO: Random RGB Generator
+function randomColor() {
+  return Math.floor(Math.random() * 255);
+}
+function randomRGB() {
+  let red = randomColor();
+  let green = randomColor();
+  let blue = randomColor();
+  return `rgb(${red},${green},${blue})`;
+}
+//TODO: Function to create color div
+function appendTiles(numToAppend) {
+  let colorArr = [];
+  for (let i = 0; i < numToAppend; i++) {
+    colorArr.push(randomRGB());
+    let colorTile = $(
+      `<div class = 'colorTile' style='background: ${colorArr[i]}'</div>`
+    );
+    $("#tiles").append(colorTile);
+  }
+  getWinningColor(colorArr);
+  console.log(colorArr);
+}
+
+//TODO: Get the winning color
+function getWinningColor(arr) {
+  $("#clue").text(arr[Math.floor(Math.random() * arr.length)]);
+}
+//TODO: Score / Round Counter
+//TODO: Increase Difficulty
