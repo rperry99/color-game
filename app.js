60b7e8fa9bdfac834c7c128a8438a24be6a60e2c
Russ Perry <rperry1886@gmail.com>
2020-07-25 22:15:11 -0400

    Add LocalHost Functionality

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
   let score = 0;
   let appendNum = 4; //Base number of tiles per game. Number will increase as score increases
 
-  $("#begin").click(() => {
+  $('#begin').click(() => {
     newGame();
     newRound(appendNum);
   });
 });
 
-$(document).on("click", ".colorTile", function (e) {
-  if ($(this).attr("data-color") === winner) {
+$(document).on('click', '.colorTile', function (e) {
+  if ($(this).attr('data-color') === winner) {
     score++;
     updateScore(score);
     if (score <= 3) {
@@ -28,28 +28,29 @@ $(document).on("click", ".colorTile", function (e) {
 });
 
 function gameOver() {
-  if (highscore < score) {
+  if (score > highscore) {
     highscore = score;
+    localStorage.setItem('highscore', highscore);
   }
   score = 0;
   updateScore(score);
-  $("#start").removeClass("hide");
-  $("#game").addClass("hide");
-  $("#begin").text("Play Again?");
-  $("#message").removeClass("hide");
-  $("#highscore").html(highscore);
+  $('#start').removeClass('hide');
+  $('#game').addClass('hide');
+  $('#begin').text('Play Again?');
+  $('#message').removeClass('hide');
+  $('#highscore').html(localStorage.getItem('highscore'));
   console.log(highscore);
-  $(".help").removeClass("hide");
-  $(".container h1").removeClass("hide");
+  $('.help').removeClass('hide');
+  $('.container h1').removeClass('hide');
 }
 
 function newGame() {
   score = 0;
   updateScore(score);
-  $("#game").removeClass("hide");
-  $("#start").addClass("hide");
-  $(".help").addClass("hide");
-  $(".container h1").addClass("hide");
+  $('#game').removeClass('hide');
+  $('#start').addClass('hide');
+  $('.help').addClass('hide');
+  $('.container h1').addClass('hide');
 }
 
 //TODO: Random RGB Generator
@@ -64,17 +65,17 @@ function randomRGB() {
 }
 //TODO: Function to create color div
 function newRound(numToAppend) {
-  $("#tiles").empty();
+  $('#tiles').empty();
   let colorArr = [];
   for (let i = 0; i < numToAppend; i++) {
     colorArr.push(randomRGB());
     let colorTile = $(
       `<div class = 'colorTile' data-color='${colorArr[i]}' style='background: ${colorArr[i]}'</div>`
     );
-    $("#tiles").append(colorTile);
+    $('#tiles').append(colorTile);
   }
   winner = getWinningColor(colorArr);
-  $("#clue").text(winner);
+  $('#clue').text(winner);
   console.log(colorArr);
 }
 
@@ -84,5 +85,5 @@ function getWinningColor(arr) {
 }
 //TODO: Score / Round Counter
 function updateScore(currentScore) {
-  $("#score").text(currentScore);
+  $('#score').text(currentScore);
 }
